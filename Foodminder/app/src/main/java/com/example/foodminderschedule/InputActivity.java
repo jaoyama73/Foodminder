3b3c64687675f12d1add90434e7c897834430100
jaoyama73 <aoyama@ucsb.edu>
2020-05-31 02:49:58 -0700

    Nav Bar

---
+++ Foodminder/app/src/main/java/com/example/foodminderschedule/InputActivity.java
@@ -0,0 +1,95 @@
+package com.example.foodminderschedule;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Pair;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.foodminderschedule.R;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class InputActivity extends AppCompatActivity {
+    private static EditText item;
+    private static EditText date;
+    private int dataSize = 0;
+    private ArrayList<String>dataArray = new ArrayList<>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.input_activity);
+
+        // Initialize dataArray or receive dataArray from previous activity
+        Intent intent = getIntent();
+        dataArray.clear();
+        dataArray = intent.getStringArrayListExtra("dataArray");
+        // If received previous data -> get the number of data/items
+        if (dataArray != null) {
+            dataSize = dataArray.size();
+        }
+        // If no previous data  (opening this app for the first time) -> initialize the dataArray
+        else {
+            dataArray = new ArrayList<>();
+            dataSize = 0;
+        }
+
+
+        Button add = findViewById(R.id.button_add);
+        Button back =findViewById(R.id.button_return);
+        item = (EditText)findViewById(R.id.editText_item);
+        date = (EditText)findViewById(R.id.editText_date);
+
+
+
+
+
+
+
+        // If 'Add' button is clicked
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Pass data as String array to Calendar
+                // Format: item '~' date
+                Intent toCalendarActivity = new Intent(getApplicationContext(),CalendarForm.class);
+                toCalendarActivity.putStringArrayListExtra("dataArray",dataArray);
+                dataArray.add(String.valueOf(item.getText())+"~"+String.valueOf(date.getText()));
+                dataSize++;
+
+                // Store in SharedPreference
+                SharedPreferences dataInfo = getSharedPreferences("dataInfo", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = dataInfo.edit();
+                editor.putInt("dataSize",dataSize);
+                for(int i = 1;i<=dataSize;i++){
+                    editor.putString("data_"+Integer.toString(i),dataArray.get(i-1));
+                }
+                editor.apply();
+                //startActivity(toCalendarActivity);
+            }
+        });
+
+
+        back.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent toMainActivity = new Intent(getApplicationContext(),MainActivity.class);
+                toMainActivity.putStringArrayListExtra("dataArray",dataArray);
+                startActivity(toMainActivity);
+            }
+        });
+
+
+
+
+
+    }
+}
---
+++ Foodminder/app/src/main/java/com/example/foodminderschedule/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.foodminderschedule;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private ArrayList<String>dataArray = new ArrayList<>();
+    private int dataSize = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.navigation);
+
+
+
+        Intent intent = getIntent();
+        dataArray.clear();
+        dataArray = intent.getStringArrayListExtra("dataArray");
+        // If received previous data -> get the number of data/items
+        if (dataArray != null) {
+            dataSize = dataArray.size();
+        }
+        // If no previous data  (opening this app for the first time) -> initialize the dataArray
+        else {
+            dataArray = new ArrayList<>();
+            dataSize = 0;
+        }
+
+        BottomNavigationView bottomNav = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+        Menu menu = bottomNav.getMenu();
+        MenuItem menuItem = menu.getItem(0);
+        menuItem.setChecked(true);
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+
+                        break;
+                    case R.id.nav_calendar:
+                        Intent intent2 = new Intent(MainActivity.this,CalendarForm.class);
+                        startActivity(intent2);
+                        break;
+                    case R.id.nav_list:
+                        Intent intent3 = new Intent(MainActivity.this,InputActivity.class);
+                        startActivity(intent3);
+                        break;
+                }
+                return false;
+            }
+        });
+
+
+
+        ImageView addByText = findViewById(R.id.input_text);
+        ImageView addByCamera = findViewById(R.id.input_camera);
+        addByText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toInputActivity = new Intent(MainActivity.this, InputActivity.class);
+                toInputActivity.putStringArrayListExtra("dataArray",dataArray);
+                startActivity(toInputActivity);
+            }
+        });
+
+
+
+
+
+    }
+
+
+
+
+
+}
