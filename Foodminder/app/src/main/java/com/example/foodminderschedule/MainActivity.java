daa7a202c0c0a1d3e952ec2a6ff902c2a734bad6
jaoyama73 <aoyama@ucsb.edu>
2020-06-08 06:09:11 -0700

    finished waiting for debug

--- Foodminder/app/src/main/java/com/example/foodminderschedule/MainActivity.java
+++ Foodminder/app/src/main/java/com/example/foodminderschedule/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -32,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.navigation);
-
 
         // Receive intent from other activities (for update dataArray)
         Intent intent = getIntent();
